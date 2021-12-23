32b5f527407c1884176b8a2d2ecd45cb07431fab
Arief Maulana <ariefmaulana0808@gmail.com>
2021-12-23 13:52:21 +0700

    First Commit

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/65535/51761168292_20ed136f9f.jpg',
+            pledged: 5000,
+            target: 100000,
+            date_end: +new Date('10 January 2022'),
+            profile_photo: 'https://live.staticflickr.com/65535/51763290194_9a0ea61f28.jpg',
+            profile_name: 'Anna Watson',
+            no_pledged: 0   
+        }
+    ]
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'Arief Maulana'
-	}
+	target: document.querySelector('#root')
 });
 
 export default app;
